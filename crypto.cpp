53ceaa9f3db767760ae35b3fed1aaad7e0fb1d3a
sekla <584892986@qq.com>
2021-12-08 18:40:11 +0800

    aes correct

--- crypto.cpp
+++ crypto.cpp
@@ -6,6 +6,7 @@
 #include <osrng.h>
 #include <filters.h>
 #include <default.h>
+#include <des.h>
 #include <string>
 #include <sstream>
 #define _CRYPTO_UTIL_H_
@@ -39,6 +40,7 @@ std::string encrypt4aes(const std::string& inData,std::string& strKey)
 {
     std::string outData = "";
     std::string errMsg = "";
+    
     //密钥经过MD5运算
     const std::string Key = encryAeskey(strKey);
 
@@ -65,30 +67,33 @@ std::string encrypt4aes(const std::string& inData,std::string& strKey)
         CBC_Mode<AES>::Encryption e;  //CBC 模式加密
         e.SetKeyWithIV((byte*)Key.c_str(), iKeyLen, iv);
         //加密的关键， outData 就是加密后的数据
-        StringSource ss(inData, true, new StreamTransformationFilter(e, new StringSink(outData)));    
+        StringSource ss(inData, true, new StreamTransformationFilter(e, 
+            new HexEncoder(new StringSink(outData))));    
     }
     catch (const CryptoPP::Exception& e)
     {
         errMsg = "Encryptor throw exception!!";
         return errMsg;
     }
-
     return outData;
 }
 
 //AES解密 indata密文 strKey解密密钥
-std::string decrypt4aes(const std::string& inData, const std::string& strKey)
+std::string decrypt4aes(const std::string& inData, std::string& strKey)
 {
     std::string outData = "";
     std::string errMsg = "";
 
-    if (inData.empty() || strKey.empty()) // 判断待加密的字符串或者密钥是否为空
+    //密钥经过MD5运算
+    const std::string Key = encryAeskey(strKey);
+
+    if (inData.empty() || Key.empty()) // 判断待加密的字符串或者密钥是否为空
     {
         errMsg = "indata or key is empty!!";
         return errMsg;
     }
 
-    unsigned int iKeyLen = strKey.length();
+    unsigned int iKeyLen = Key.length();
 
     if (iKeyLen != AES_KEY_LENGTH_16 && iKeyLen != AES_KEY_LENGTH_24  //判断密钥的长度是否符合要求
         && iKeyLen != AES_KEY_LENGTH_32)
@@ -101,10 +106,12 @@ std::string decrypt4aes(const std::string& inData, const std::string& strKey)
 
     try
     {
-        CBC_Mode<AES>::Decryption d;    //CBC 模式解密
-        d.SetKeyWithIV((byte*)strKey.c_str(), iKeyLen, iv);
+        //CBC 模式解密
+        CBC_Mode<AES>::Decryption d;    
+        d.SetKeyWithIV((byte*)Key.c_str(), iKeyLen, iv);
+        //解密的函数，outData 是解密的结果
         StringSource ss(inData, true,
-            new StreamTransformationFilter(d, new StringSink(outData)));  //解密的函数，outData 是解密的结果
+            new StreamTransformationFilter(d, new HexEncoder(new StringSink(outData))));  
     }
     catch (const CryptoPP::Exception& e)
     {
@@ -116,11 +123,59 @@ std::string decrypt4aes(const std::string& inData, const std::string& strKey)
 }
 
 
+//3DES加密
+std::string	encrypt3des(std::string& inData) {
+    std::string outData;
+    std::string printKey;
+    //随机key
+    AutoSeededRandomPool prng;
+    SecByteBlock key(0x00, DES_EDE2::DEFAULT_KEYLENGTH);
+    prng.GenerateBlock(key, key.size());
+
+    //生成iv
+    byte iv[DES_EDE2::BLOCKSIZE];
+    prng.GenerateBlock(iv, sizeof(iv));
+    
+    StringSource pkey(key, true,
+        new HexEncoder(
+            new StringSink(printKey)));
+
+
+    try {
+        ECB_Mode<DES_EDE3>::Encryption e;
+        e.SetKeyWithIV(key, key.size(), iv);
+
+        //ECB和CBC模式必须填充加密块
+        StringSource ss(inData, true, new 
+            StreamTransformationFilter(e,new StringSink(outData)));
+    }
+    catch (const Exception& e) {
+        return e.what();
+    }
+
+    return outData;
+}
+
+//3DES解密
+std::string decrypt3des(const std::string& key, std::string& inData,const std::string iv) {
+    std::string outData;
+    try {
+        CBC_Mode<DES_EDE3>::Decryption e;
+        e.SetKeyWithIV((byte*)key.c_str(), key.size(),(byte*)iv.c_str());
+        StringSource ss(inData, true, new 
+            StreamTransformationFilter(e, new StringSink(outData)));
+    }
+    catch (const Exception& e) {
+        return e.what();
+    }
+    return outData;
+}
+
 PYBIND11_MODULE(pycryptodll, m) {
     m.doc() = "crypto++";
 	m.def("retMD5", &retMD5, "return the MD5 value");
     
     m.def("enAESkey", &encryAeskey, "encry the AES key");
-    m.def("enAES", &encrypt4aes, "encry the AES", py::arg("indata"), py::arg("key"));
+    m.def("enAES", &encrypt4aes, "encrypt the AES", py::arg("indata"), py::arg("key"));
     m.def("deAES", &decrypt4aes, "decode the AES", py::arg("indata"), py::arg("key"));
 }
